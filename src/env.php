3d02c4bf83b409ccc5b230c2d3991f0cbd69a406
GZod01 <gzod01@gzod01.fr>
2026-03-13 08:57:59 +0100

    h

--- src/env.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_usr = "";
-$db_pass = "";
-$db_db = "makernight_leaderboard";
-$db_port = 3306;
-if(file_exists("env_override.php")){
-    include "env_override.php";
-}
